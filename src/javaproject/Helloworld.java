7c83cec3e445ba12355fd6ca2fe1d6627c47d397
22jy0125 <22jy0125@PC02.jynet2.jec.ac.jp>
2023-10-25 09:22:23 +0900

    ループ回数の増加

--- src/javaproject/Helloworld.java
+++ src/javaproject/Helloworld.java
@@ -2,7 +2,7 @@ package javaproject;
 
 public class Helloworld {
 	public static void main(String[] args) {
-		for(int i = 0; i<3; i++) {
+		for(int i = 0; i<4; i++) {
 			System.out.println("Hello world12345");
 		}
 		System.out.println("54321dlrow olleH");
